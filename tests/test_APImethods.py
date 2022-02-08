2fd0e5d5f802dc3b4774280feaaa144a19180300
Oleg <saga6021@gmail.com>
2022-02-08 09:06:53 +0700

    Added some comments

--- tests/test_APImethods.py
+++ tests/test_APImethods.py
@@ -2,16 +2,19 @@ import pytest
 import requests
 
 
+# Testing GET method structure
 def test_GETmethods():
     respond = requests.get("http://127.0.0.1:17678/api/state")
     assert respond.json() == {'statusCode': 0, 'state': 'OК'}
 
 
+# Testing OPTION method structure
 def test_OPTIONSmethods():
     respond = requests.options("http://127.0.0.1:17678")
     assert respond.headers["Access-Control-Request-Method"] == 'POST, GET, OPTIONS'
 
 
+# Testing POST methods structure
 @pytest.mark.parametrize("x, y, task, expected_response", [(10, 2, "addition", {'statusCode': 0, 'result': 12}),
                                                            (5, 2, "multiplication", {'statusCode': 0, 'result': 10}),
                                                            (7, 2, "division", {'statusCode': 0, 'result': 3}),
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import pytest
 from subprocess import PIPE, Popen
 
 
+# Getting stdout and executing command
 def cmdline(command):
     process = Popen(
         args=command,
@@ -11,6 +12,7 @@ def cmdline(command):
     return process.communicate()[0]
 
 
+# Testing defaults parameters (start, stop, restart)
 @pytest.mark.parametrize("str, output", [
     ("cd .. & webcalculator.exe start", "Сервер уже запущен\r\n"),
     ("cd .. & webcalculator.exe restart", "Пытаемся остановить Веб-калькулятор\r\nВеб-калькулятор остановлен\r\n"
@@ -22,6 +24,7 @@ def test_launching(str, output):
     assert cmdline(str).decode('cp1251') == output
 
 
+# Testing optional parameters host, port (star [host] [port])
 @pytest.mark.parametrize("str, output", [
     ("cd .. & webcalculator.exe start 0.0.0.0", "Запуск Веб-калькулятора на 0.0.0.0:17678\r\n"
                                                 "Веб-калькулятор запущен на 0.0.0.0:17678\r\n"),
