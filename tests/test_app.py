32f1abc02aedce366ef338e8cadefa2589a4273b
Oleg Kanaev <saga6021@gmail.com>
2022-04-10 21:51:09 +0700

    Added allure report. Tried all it's features

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,27 +1,32 @@
+import allure
 import requests
 from conftest import *
-from config import HOST, PORT
+from tests.config import HOST, PORT
 
 
 # Testing defaults parameters (start, stop, restart)
-@pytest.mark.parametrize("str, output", [
+@allure.feature("CLI check")
+@pytest.mark.parametrize("shell_command, result", [
     ("cd .. & webcalculator.exe start", "Сервер уже запущен\r\n"),
     ("cd .. & webcalculator.exe restart", "Пытаемся остановить Веб-калькулятор\r\nВеб-калькулятор остановлен\r\n"
                                           "Запуск Веб-калькулятора на 127.0.0.1:17678\r\n"
                                           "Веб-калькулятор запущен на 127.0.0.1:17678\r\n"),
     ("cd .. & webcalculator.exe stop", "Пытаемся остановить Веб-калькулятор\r\nВеб-калькулятор остановлен\r\n"),
-    ("cd .. & webcalculator.exe restart", 'Веб-калькулятор не запущен. Используйте команду "start"\r\n')])
-def test_launching(str, output):
-    try:
-        respond = requests.get(f"http://{HOST}:{PORT}/api/state").json()
-        assert cmdline(str).decode('cp1251') == output and respond == {'statusCode': 0, 'state': 'OК'}
-    except Exception as e:
-        print(e)
-        assert cmdline(str).decode('cp1251') == output
+    ("cd .. & webcalculator.exe restart", 'Веб-калькулятор не запущен. Используйте команду "start"\r\n')],
+    ids=["Калькулятор уже запущен", "Калькулятор рестартанул", "Калькулятор остановлен", "Калькулятор не запущен"])
+def test_launching(shell_command, result):
+    with allure.step(f"Запущен на {HOST}:{PORT}"):
+        try:
+            respond = requests.get(f"http://{HOST}:{PORT}/api/state").json()
+            assert cmdline(shell_command).decode('cp1251') == result and respond == {'statusCode': 0, 'state': 'OК'}
+        except Exception as e:
+            print(e)
+            assert cmdline(shell_command).decode('cp1251') == result
 
 
 # Testing optional parameters host, port (star [host] [port])
-@pytest.mark.parametrize("str_, host, port, output", [
+@allure.feature("CLI check")
+@pytest.mark.parametrize("shell_command, host, port, result", [
     ("cd .. & webcalculator.exe start 0.0.0.0", "0.0.0.0", f"{PORT}", "Запуск Веб-калькулятора на 0.0.0.0:17678\r\n"
                                                                     "Веб-калькулятор запущен на 0.0.0.0:17678\r\n"),
     ("cd .. & webcalculator.exe stop", f"{HOST}", f"{PORT}", "Пытаемся остановить Веб-калькулятор\r\n"
@@ -29,16 +34,17 @@ def test_launching(str, output):
     ("cd .. & webcalculator.exe start 127.5.5.5 5555", "127.5.5.5", "5555",
      "Запуск Веб-калькулятора на 127.5.5.5:5555\r\nВеб-калькулятор запущен на 127.5.5.5:5555\r\n"),
     ("cd .. & webcalculator.exe stop", f"{HOST}", f"{PORT}", "Пытаемся остановить Веб-калькулятор\r\n"
-                                                        "Веб-калькулятор остановлен\r\n")
-    ])
+                                                        "Веб-калькулятор остановлен\r\n")],
+    ids=["Калькулятор запущен", "Калькулятор остановлен", "Калькулятор запущен", "Калькулятор остановлен"])
 @pytest.mark.usefixtures("setup_for_CLI_test_launching_with_params")
-def test_launching_with_params(str_, host, port, output):
-    try:
-        respond = requests.get(f"http://{host}:{port}/api/state").json()
-        assert cmdline(str_).decode('cp1251') == output and respond == {'statusCode': 0, 'state': 'OК'}
-    except Exception as e:
-        print(e)
-        assert cmdline(str_).decode('cp1251') == output
+def test_launching_with_params(shell_command, host, port, result):
+    with allure.step(f"Запущен на {host}:{port}"):
+        try:
+            respond = requests.get(f"http://{host}:{port}/api/state").json()
+            assert cmdline(shell_command).decode('cp1251') == result and respond == {'statusCode': 0, 'state': 'OК'}
+        except Exception as e:
+            print(e)
+            assert cmdline(shell_command).decode('cp1251') == result
 
 
 if __name__ == "__main__":
--- tests/test_error_codes.py
+++ tests/test_error_codes.py
@@ -1,15 +1,18 @@
 import pytest
+import allure
 import requests
-from config import HOST, PORT
+from tests.config import HOST, PORT
 
 
 # Test GET method raising statusCode:5
+@allure.feature("ERROR=5 check")
 def test_get_error_code_5():
     respond = requests.post(f"http://{HOST}:{PORT}/api/state", json={})
     assert respond.json() == {"statusCode": 5, "statusMessage": " state - Не верное имя задачи или тип HTTP запроса"}
 
 
 # Test POST methods raising statusCode:5 with JSON format error
+@allure.feature("ERROR=5 check")
 @pytest.mark.parametrize("task", ["addition", "multiplication", "division", "remainder"])
 def test_post_error_code_5_1(task):
     respond = requests.post(f"http://{HOST}:{PORT}/api/{task}", data={b"some data"})
@@ -17,6 +20,7 @@ def test_post_error_code_5_1(task):
 
 
 # Test POST methods raising statusCode:5 with HTTP method or task name error
+@allure.feature("ERROR=5 check")
 @pytest.mark.parametrize("task", ["my_own_task_lol", "addition", "multiplication", "division", "remainder"])
 def test_post_error_code_5_2(task):
     respond = requests.get(f"http://{HOST}:{PORT}/api/{task}", json={})
@@ -24,6 +28,7 @@ def test_post_error_code_5_2(task):
 
 
 # Test POST methods raising statusCode:2 Required parameters are not specified
+@allure.feature("ERROR=2 check")
 @pytest.mark.parametrize("task", ["addition", "multiplication", "division", "remainder"])
 @pytest.mark.parametrize("json", [{"x": 5}, {"y": 5}, {}, {"x": 5, }])
 def test_post_error_code_2(task, json):
